80d1bbe74285e848b33468c0f6b5b3dc66c16131
glenn <ghinks@yahoo.com>
2019-11-14 09:30:51 -0500

    add some print statements to work out file path issues add current work items

--- readme.md
+++ readme.md
@@ -4,3 +4,7 @@
 In order to further some basic understanding of how to use Machine 
 Learning I am creating this project as vehicle to learn.
 
+## Current Items
+- naming conventions for files etc, read pep
+- unit test coverage, run all unit tests from the command line
+- find out the , remind your fucking self , what __init__.py does
--- src/parsers/csv/testCSVParser.py
+++ src/parsers/csv/testCSVParser.py
@@ -1,12 +1,20 @@
+import sys
+import os
+from pathlib import Path
 import unittest
 from src.parsers.csv.csvDataParser import CSVReader
+
 
 class TestCSVParser(unittest.TestCase):
     def test_something(self):
         # TODO why the full path only, why not relative
-       reader = CSVReader("/home/glenn/dev/epl-predictor/data/eplCSV2000-2018/data.csv")
-       results = reader.read()
-       self.assertEqual(results.empty, False)
+        print(Path(__file__))
+        print(os.path.dirname(os.path.abspath(__file__)))
+        print(os.path.dirname(sys.modules['__main__'].__file__))
+        print(os.path.abspath(os.curdir))
+        reader = CSVReader("/home/glenn/dev/epl-predictor/data/eplCSV2000-2018/data.csv")
+        results = reader.read()
+        self.assertEqual(results.empty, False)
 
 
 if __name__ == '__main__':
