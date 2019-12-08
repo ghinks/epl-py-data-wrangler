3124afab2725d12ef043fe16cbaab89fc6cf6c72
glenn <ghinks@yahoo.com>
2019-12-08 13:36:16 -0500

    add a main function fix the date tests

--- src/parsers/reader/test_reader.py
+++ src/parsers/reader/test_reader.py
@@ -15,19 +15,40 @@ class testReader(unittest.TestCase):
         self.assertIsNotNone(pyDate)
         self.assertEqual(datetime.date(2000, 12, 13), pyDate)
 
-    def test_date_conversion_form_one_year_two_digits(self):
-        baseReader = reader.Reader("")
-        test_date = "13/12/03"
-        pyDate = baseReader.convertStrDate(test_date)
-        self.assertIsNotNone(pyDate)
-        self.assertEqual(datetime.date(2003, 12, 13), pyDate)
-
     def test_date_conversion_form_two(self):
         baseReader = reader.Reader("")
         test_date = "2000-12-13"
         pyDate = baseReader.convertStrDate(test_date)
         self.assertIsNotNone(pyDate)
         self.assertEqual(datetime.date(2000, 12, 13), pyDate)
+
+    def test_date_conversion_form_two(self):
+        baseReader = reader.Reader("")
+        test_date = "15/08/00"
+        pyDate = baseReader.convertStrDate(test_date)
+        self.assertIsNotNone(pyDate)
+        self.assertEqual(datetime.date(2000, 8, 15), pyDate)
+
+    def test_date_conversion_form_two(self):
+        baseReader = reader.Reader("")
+        test_date = "15/08/98"
+        pyDate = baseReader.convertStrDate(test_date)
+        self.assertIsNotNone(pyDate)
+        self.assertEqual(datetime.date(1998, 8, 15), pyDate)
+
+    def test_date_conversion_ddmmyyyy(self):
+        baseReader = reader.Reader("")
+        test_date = "21/04/2003"
+        pyDate = baseReader.convertStrDate(test_date)
+        self.assertIsNotNone(pyDate)
+        self.assertEqual(datetime.date(2003, 4, 21), pyDate)
+
+    def test_date_conversion_ddmmyy(self):
+        baseReader = reader.Reader("")
+        test_date = "11/05/02"
+        pyDate = baseReader.convertStrDate(test_date)
+        self.assertIsNotNone(pyDate)
+        self.assertEqual(datetime.date(2002, 5, 11), pyDate)
 
     def test_date_conversion_form_bad_format(self):
         baseReader = reader.Reader("")
